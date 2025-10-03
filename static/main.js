6eff27fc640c45647c1dd1c644f85dd5b3a5a862
UNICDEB <doloidebabrat99@gmail.com>
2025-10-03 17:23:12 +0530

    Version 1.0.0.3

--- static/main.js
+++ static/main.js
@@ -1,8 +1,46 @@
+// function startCamera() {
+//   fetch("/start-camera/", { method: "POST" })
+//     .then(response => response.json())
+//     .then(data => alert(data.status))
+//     .catch(err => alert("Error starting camera"));
+// }
+
+// function showSpinner() {
+//   document.getElementById("spinner").style.display = "block";
+// }
+
+// function clearResults() {
+//   // Hide spinner if visible
+//   document.getElementById("spinner").style.display = "none";
+//   // Remove results
+//   const resultDiv = document.getElementById("results");
+//   if (resultDiv) {
+//     resultDiv.innerHTML = "";
+//   }
+// }
+
 function startCamera() {
   fetch("/start-camera/", { method: "POST" })
     .then(response => response.json())
-    .then(data => alert(data.status))
+    .then(data => {
+      alert(data.status);
+      if (data.status === "Camera started") {
+        const v = document.getElementById('video_stream');
+        if (v) v.style.display = 'block';
+      }
+    })
     .catch(err => alert("Error starting camera"));
+}
+
+function stopCamera() {
+  fetch("/stop-camera/", { method: "POST" })
+    .then(response => response.json())
+    .then(data => {
+      alert(data.status);
+      const v = document.getElementById('video_stream');
+      if (v) v.style.display = 'none';
+    })
+    .catch(err => alert("Error stopping camera"));
 }
 
 function showSpinner() {
@@ -11,8 +49,10 @@ function showSpinner() {
 
 function clearResults() {
   // Hide spinner if visible
-  document.getElementById("spinner").style.display = "none";
-  // Remove results
+  const spinnerDiv = document.getElementById("spinner");
+  if (spinnerDiv) spinnerDiv.style.display = "none";
+
+  // Remove results area content
   const resultDiv = document.getElementById("results");
   if (resultDiv) {
     resultDiv.innerHTML = "";
